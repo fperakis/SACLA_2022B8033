22e4ba23261f44712164f0d5f687fe97bd49607f
Fivos <f.perakis@fysik.su.se>
2022-10-25 18:00:13 +0900

    Update utilities.py

--- src/utilities.py
+++ src/utilities.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 def do_histogram(a,bi,bf,db):
     '''
@@ -9,3 +10,23 @@ def do_histogram(a,bi,bf,db):
     y,x_tmp = np.histogram(a,bins=bins)
     x = np.array([(bins[j]+bins[j+1])/2. for j in range(len(bins)-1)])
     return x,y
+
+def line(x,a,b):
+    return a*x+b
+
+def gaussian(x,a,b,c,d):
+    # in this form the c is the FWHM
+    return np.abs(a)*np.exp(-4*np.log(2)*(x-b)**2./(c**2))+d
+
+def exponential(x,a,b,c):
+    return np.abs(a)*np.exp(-x/(np.abs(b)))+c
+
+def fit(function,x,y,p0=None,sigma=None,bounds=None):
+    '''
+    fits a function and return the fit resulting parameters and curve
+    '''
+    popt,pcov = curve_fit(function,x,y,p0=p0,sigma=sigma)
+    #x = np.arange(0,1e4)
+    curve = function(x,*popt)
+    perr = np.sqrt(np.diag(pcov))
+    return popt,x,curve,perr
