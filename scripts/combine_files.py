159c41c3a5160a972fee7f33a1f067e4c8101ec4
AnitaGirelli <girelli.anita@gmail.com>
2022-10-28 20:49:14 +0900

    combine_files.py that works

--- scripts/combine_files.py
+++ scripts/combine_files.py
@@ -1,7 +1,10 @@
 import numpy as np
 import h5py
 import os
+import glob
+import os
 import sys
+
 
 # parameters for loading
 path_combined='/UserData/maddalena/sacla2018/06-Iq_combined/'
@@ -10,25 +13,19 @@ path='/UserData/maddalena/sacla2018/03-h5analysis/'
 
 
 def combine_files(run):
-    a=os.popen(f'cd '+path+';ls IqPhi_{run}_*')
-    string=a.read()
+    os.chdir("/UserData/maddalena/sacla2018/03-h5analysis/")
+    files = glob.glob(f"IqPhi_{run}*.h5")
 
-    string=string.partition('\n')[2]
-    I=[]
-    while string!='':
-            file=string.partition('\n')[0]
-            f=h5py.File(pathSave+string.partition('\n')[0],'r')
-            I+=f['I']
-            print(np.shape(I2d))
-            string=string.partition('\n')[2]
-
-
-
+    f=h5py.File(files[0],'r')
     q=f['q'][:]
     Phi=f['phi'][:]
+    I=[]
+    
+    for file in files:
+        f=h5py.File(path+file,'r')
+        I+=f['I']    
 
-
-    hf = h5py.File(pathSave+f'{run}_IqPhi.h5', 'w')
+    hf = h5py.File(path_combined+f'IqPhi_{run}.h5', 'w')
     hf.create_dataset('q', data=q)
     hf.create_dataset('I', data=I)
     hf.create_dataset('phi', data=I)
