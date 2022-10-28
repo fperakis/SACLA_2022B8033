91bbffed6733431d06675e9739255f1c98274065
maddalenabin <maddalenabin@gmail.com>
2022-10-29 00:34:43 +0900

    fixed minor bugs

--- scripts/combine_files.py
+++ scripts/combine_files.py
@@ -1,31 +1,29 @@
 import numpy as np
 import h5py
-import os
 import glob
 import os
 import sys
-
+import time
 
 # parameters for loading
-path_combined='/UserData/maddalena/sacla2018/06-Iq_combined/'
-path='/UserData/maddalena/sacla2018/03-h5analysis/'
-
-
+path_combined = '/UserData/maddalena/sacla2022/06-Iq_combined/'
+path = '/UserData/maddalena/sacla2022/03-h5analysis/'
 
 def combine_files(run):
-    os.chdir("/UserData/maddalena/sacla2018/03-h5analysis/")
+    os.chdir(path)
     files = glob.glob(f"IqPhi_{run}*.h5")
 
-    f=h5py.File(files[0],'r')
-    q=f['q'][:]
-    Phi=f['phi'][:]
-    I=[]
+    f = h5py.File(files[0],'r')
+    q = f['q'][:]
+    Phi = f['phi'][:]
+    I = []
     
-    for file in files:
-        f=h5py.File(path+file,'r')
-        I+=f['I']    
+    for file in files[1:]:
+        f = h5py.File(path+file,'r')
+        I += f['I']    
 
-    hf = h5py.File(path_combined+f'IqPhi_{run}.h5', 'w')
+    timestamp = time.strftime("%d%H%M%S", time.gmtime(time.time()))
+    hf = h5py.File(path_combined + f'IqPhi_{run}_{timestamp}.h5', 'w')
     hf.create_dataset('q', data=q)
     hf.create_dataset('I', data=I)
     hf.create_dataset('phi', data=I)
